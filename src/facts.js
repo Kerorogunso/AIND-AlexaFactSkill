b9bb06a4d748ad8be0e58e4b3877f6a6fbd73f00
Albert <albertswchung@gmail.com>
2018-03-15 11:37:55 +0000

    Alexa

---
+++ src/facts.js
@@ -0,0 +1,17 @@
+// TODO Add more or replace entirely the facts below for your chosen history domain
+// Include at least 10 distinct facts with a 4-digit year included in the text for each
+module.exports = {
+    "FACTS_EN": [
+        "The field of AI is considered to have its origin in 1950, with publication of British mathematician Alan Turing's paper, Computing Machinery and Intelligence.",
+        "The term, Artificial Intelligence, was coined in 1956 by mathematician and computer scientist John McCarthy, at Dartmouth College, in New Hampshire.",
+        "In 1997, a chess-playing program named Deep Blue, developed by IBM, beat the reigning world chess champion.",
+        "A so-called AI Winter occurred in 1974, when funding was cut after Speech Understanding research did not live up to its promise.",
+        "A driverless robotic car named Stanley, engineered by Sebastian Thrun's Stanford Racing team, sped through the Mojave desert at 22 miles per hour to win the 2005 Darpa Grand Challenge.",
+        "Backpropagation was first proposed in 1969, but it only became part of mainstream machine learning in the mid-1980s.",
+        "In March 2016, AlphaGo used deep learning to beat one of the highest ranked players Lee Sedol in the game of Go.",
+        "In the mid-1960s, a professor at the MIT Artificial Intelligence Laboratory developed a computer psychotherapist called ELIZA, which could carry out seemingly intelligent conversations via text with users.",
+        "In January 2017, Carnegie Mellon University developed Liberatus to play against professional Poker players in no-limit Texas hold\'em. The bot was 1 million dollars ahead of the human players in chips.",
+        "IBM\'s Watson computer defeated television game show Jeopardy! champions Rutter and Jennings in 2011."
+
+    ]
+}
